6f26d6748173ff040af156f64762b9d344d367c5
Libertrendz <contato@libertrendz.eu>
2026-02-09 15:05:08 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <section className="space-y-4">
+      <h1 className="text-3xl font-semibold text-figueira">
+        Mais que uma igreja, uma família
+      </h1>
+
+      <p className="text-muted max-w-xl">
+        Bem-vindo ao site oficial da Assembleia de Deus – Ministério Visão de Conquista.
+      </p>
+    </section>
+  );
+}
