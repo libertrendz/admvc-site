4118138dabe07bbed3473a780e3cf91b6dcfa3c9
Libertrendz <contato@libertrendz.eu>
2026-02-09 12:48:55 +0000

    Create page.tsx

---
+++ app/sobre/page.tsx
@@ -0,0 +1,9 @@
+// app/sobre/page.ts
+export default function Page() {
+  return (
+    <section className="space-y-2">
+      <h1 className="text-2xl font-semibold">Em construção</h1>
+      <p className="text-muted">Conteúdo será publicado em breve.</p>
+    </section>
+  );
+}
